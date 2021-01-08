9e35bf837970efc46a3fbcf54a1e632377deca61
ernestoneyra <ernestoneyra31@gmail.com>
2021-01-08 18:46:17 +0100

    got the info från the API

--- Labb 3/filmsite.js
+++ Labb 3/filmsite.js
@@ -1,4 +1,4 @@
-    
+    //Variables
     let searchfield = document.querySelector('#searchfield');
     let api_key = "44b7d64b";
     let search = (searchfield.value);
@@ -7,22 +7,22 @@
     let url = `http://www.omdbapi.com/?apikey=${api_key}&s${search}`;
     let url2 = `http://www.omdbapi.com/?s=${search}&apikey=${api_key}`;
     let url3 = `http://www.omdbapi.com/?t=${search}&apikey=${api_key}`;
+    // End of variables
 
 
+    //Search function
     async function searchFunc(search){
         let response = await fetch(`http://www.omdbapi.com/?s=${search}&apikey=${api_key}`)
         .then(response => response.json())
          return response;
        
     }
-
-    console.log(searchfield.value.length)
-    console.log(searchfield.value);
+    // End of search function
+   
 
   
 
     // Search results
-    
     searchfield.addEventListener('keyup', function(e){
         if (e.target.value.length > 2){
             let movie = searchFunc(e.target.value) // kör JSON fetch functionen om det finns mer än 2 karaktärer i sökfältet.
@@ -36,34 +36,33 @@
                     }else{
                         img.setAttribute('src', './images/ni_image.jpg')
                     }
-                    //document.querySelector('.caro').remove('.caro');
+                  
                     img.setAttribute('class', movie.Title);
                     img.setAttribute('alt', movie.Title);
                     movie_results.appendChild(img);
                     console.log(img);
+                    // End of search result
+
+
+                    // More info about the movie
+                    async function getInfo(){
+                        fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=${api_key}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        alert(`Actors: ${data.Actors} \nYear: ${data.Year} \nPlot: ${data.Plot}`);
+                       
+                    })
                     
-                    //More info about the movie
-                    //img.addEventListener('click', () =>{
-                        //console.log(movie.Title);
-                        /*fetch(`http://www.omdbapi.com/?s=${search}&apikey=${api_key}`)
-                        .then(response => response.json())
-                        console.log(movie.imdbID)
-                        return movie.imdbID;*/
-                        /*.then(data => {
-                            console.log(data.imdbID);
-                        })*/
-        
-                    //})
-                        async function getInfo(movie){
+                    return movie;
+                    }
+                    img.addEventListener('click', ()=>
+                        {
                             fetch(`http://www.omdbapi.com/?s=${search}&apikey=${api_key}`)
-                        .then(response => response.json())
-                        console.log(movie.imdbID)
-                        return movie.imdbID;
-                        }
-                            img.addEventListener('click', ()=>{
-                                getInfo(movie)
-                                
-                            })
+                            .then(response => response.json())
+                            console.log(movie.imdbID)
+                            return getInfo();
+                        })
+                    /// End of movie info
 
                 }
             })
